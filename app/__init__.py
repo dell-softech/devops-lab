7f78a780925292fbf6972a0e431b0fd3f0e6b1aa
dell-softech <dellsoftech@gmail.com>
2026-09-02 15:05:27 +0500

    Initial Flask application

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return "Hello DevOps!"
+
+    @app.route("/health")
+    def health():
+        return {"status": "ok"}
+
+    return app
